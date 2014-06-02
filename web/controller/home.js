637286da32ee4f0bfcd839cd0a5088bb85e0f2c4
Tony Abou-Assaleh <taa@titanfile.com>
2014-06-01 23:27:08 -0400

    Update home.js

--- web/controller/home.js
+++ web/controller/home.js
@@ -1,3 +1,20 @@
 App.controller('home', function (page) {
 	// put stuff here
+	$(page)
+    .find('.app-button')
+    .on('click', function () {
+      var message = $('#input-message').val();
+      kik.send({
+  			title: 'Incoming Message!',
+  			text: 'This will self-destruct 3 seconds after opening.',
+  			data: {'text': message}
+			});
+    });
+    if (kik.message) {
+		  $('#message').show();
+		  $('#message .message-text').text(kik.message.text);
+		  setTimeout(function () {
+		    $('#message').hide();
+		  }, 3000); // Destroy message after 3 seconds.
+		}
 });
